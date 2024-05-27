60ae886675fe1fe4e7e7e543976d8d39dce0a3d0
Gabriela Tomaz do Amaral Ribeiro <32981034+gabrielatomaz@users.noreply.github.com>
2024-05-27 10:24:30 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        var gamePad = new Gamepad();
+        var smartController = new SmartController(gamePad);
+        var button = new Button();
+        var subsystemAnd = new Subsystem("AND SAMPLE");
+        var subsystemOr = new Subsystem("OR SAMPLE");
+
+
+        var lastUpdateTime = System.currentTimeMillis();
+        var updateInterval = 5000;
+        gamePad.a = true;
+        gamePad.b = true;
+        gamePad.x = true;
+
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastUpdateTime >= updateInterval) {
+                System.out.println("5s");
+                gamePad.a = !gamePad.a;
+                gamePad.b = !gamePad.b;
+
+                lastUpdateTime = currentTime;
+            }
+
+            button.whenPressed(smartController.getButtonA())
+                    .and(smartController.getButtonB())
+                    .then(subsystemAnd::execute);
+
+            button.whenPressed(smartController.getButtonA())
+                    .or(smartController.getButtonX())
+                    .then(subsystemOr::execute);
+       }
+    }
+}
